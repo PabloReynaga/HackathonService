3231b832aa60e45f5c77a1190b6ab5e163b5a394
Fabrizio Damicelli <fabridamicelli@gmail.com>
2025-09-06 14:26:53 +0200

    add skeleton of preprocessor

--- app/services/preprocessor/preprocessor.py
+++ app/services/preprocessor/preprocessor.py
@@ -1,1 +1,18 @@
-#Preprocessing service
+MODEL = "model-name"
+
+
+def embed_sentence(s: str, model) -> list[float]: ...
+
+
+def save_embedding(emb: list[float], db): ...
+
+
+def preprocess_line(s: str) -> str: ...
+
+
+def preprocess_file(filepath, db):
+    with open(filepath) as file:
+        for line in file:
+            sentence = preprocess_line(line)
+            emb = embed_sentence(sentence, model=MODEL)
+            save_embedding(emb, db)
